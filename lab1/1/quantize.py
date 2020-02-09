05a88646400c57ce1bc31fa728defc5e823e45e1
Ersain Dynmukhamed <37865517+ErsainKRX@users.noreply.github.com>
2020-02-09 12:51:01 +0300

    Update quantize.py

--- lab1/1/quantize.py
+++ lab1/1/quantize.py
@@ -1,26 +1,14 @@
 import cv2
-import numpy as np
+from PIL import Image
+import PIL
 
 img = cv2.imread('../labka.jpg')
 
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 cv2.imwrite('grayscale_labka.jpg', img_gray)
 
-img_gray = cv2.imread('grayscale_labka.jpg')
+im1 = Image.open('grayscale_labka.jpg')
 
-z = img_gray.reshape((-1, 3))
+im1 = im1.quantize(6)
 
-z = np.float32(z)
-crit = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-k = 6
-ret, label, center = cv2.kmeans(z, k, None, crit, 10, cv2.KMEANS_RANDOM_CENTERS)
-
-center = np.uint8(center)
-res = center[label.flatten()]
-res2 = res.reshape(img_gray.shape)
-
-cv2.imshow('res2', res2)
-key = cv2.waitKey(0) & 0xFF
-
-if key == '27':
-    cv2.destroyAllWindows()
+im1.show()
