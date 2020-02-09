d032d9ca3da626f54d63365fcb18544150f7636a
ydynmukhamed <dynmukhamed.ye@chocofood.kz>
2020-02-09 14:20:36 +0600

    Add colorspace conversions

---
+++ convert_to_different_colors.py
@@ -0,0 +1,15 @@
+import cv2
+
+img = cv2.imread('labka.jpg')
+
+yuv = cv2.cvtColor(img, cv2.COLOR_BGR2YUV)
+hls = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
+
+cv2.imshow('original', img)
+cv2.imshow('YUV', yuv)
+cv2.imshow('HLS', hls)
+
+k = cv2.waitKey(0) & 0xFF
+
+if k == '27':
+    cv2.destroyAllWindows()
