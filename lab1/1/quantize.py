bfcec1771ad5380e98e0003d600ae2bf5c7f0d37
Ersain Dynmukhamed <37865517+ErsainKRX@users.noreply.github.com>
2020-02-09 12:57:08 +0300

    Update quantize.py

--- lab1/1/quantize.py
+++ lab1/1/quantize.py
@@ -1,14 +1,24 @@
 import cv2
 from PIL import Image
-import PIL
 
 img = cv2.imread('../labka.jpg')
 
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 cv2.imwrite('grayscale_labka.jpg', img_gray)
 
-im1 = Image.open('grayscale_labka.jpg')
+gray_img = Image.open('grayscale_labka.jpg')
 
-im1 = im1.quantize(6)
+quan2 = gray_img.quantize(2)
+quan4 = gray_img.quantize(4)
+quan6 = gray_img.quantize(6)
 
-im1.show()
+quan2.show('2 colors')
+quan4.show('4 colors')
+quan6.show('6 colors')
+
+cv2.imshow('original', img)
+
+k = cv2.waitKey(0) & 0xFF
+
+if k == '27':
+    cv2.destroyAllWindows()
